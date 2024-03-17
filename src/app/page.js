d7a1f0c6fad54d4a7ba6ad7aa49aee86235f6f60
Numidian Dev <scottreeves250@gmail.com>
2024-03-17 15:48:36 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/header/header";
 import Hero from "@/components/hero/hero";
+import TheGame from "@/components/theGame/theGame";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <TheGame />
     </main>
   );
 }
---
+++ src/components/theGame/theGame.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const TheGame = () => {
+    return (
+        <section id='the-game'>
+            <div className="title-page">About the game</div>
+            <div className="container-theGame">
+                <div className="contents-game">
+                    <div className="content-img">
+                        <img src="/assets/theGame/3.jfif" alt="" />
+                    </div>
+                    <div className="content-p">
+                        <h1>Content title</h1>
+                        <p>Lorem ipsum dolor sit amet. Et incidunt sequi aut adipisci enim sed quia
+                            eius et nemo possimus qui laboriosam incidunt non quisquam cupiditate quo
+                            omnis dolores. Est laboriosam dolore et assumenda numquam At numquam porro
+                            hic voluptas odit ut quas reiciendis ea libero consequatur. Est quia ullam qui
+                            unde quasi aut omnis internos! Lorem ipsum dolor sit amet. Et incidunt sequi aut adipisci enim sed quia
+                            eius et nemo possimus qui laboriosam incidunt non quisquam cupiditate quo
+                            omnis dolores. Est laboriosam dolore et assumenda numquam At numquam porro
+                            hic voluptas odit ut quas reiciendis ea libero consequatur. Est quia ullam qui
+                            unde quasi aut omnis internos!</p>
+                    </div>
+                </div>
+                <div className="contents-game">
+                    <div className="content-img">
+                        <img src="/assets/theGame/2.jfif" alt="" />
+                    </div>
+                    <div className="content-p">
+                        <h1>Content title</h1>
+                        <p>Lorem ipsum dolor sit amet. Et incidunt sequi aut adipisci enim sed quia
+                            eius et nemo possimus qui laboriosam incidunt non quisquam cupiditate quo
+                            omnis dolores. Est laboriosam dolore et assumenda numquam At numquam porro
+                            hic voluptas odit ut quas reiciendis ea libero consequatur. Est quia ullam qui
+                            unde quasi aut omnis internos! Lorem ipsum dolor sit amet. Et incidunt sequi aut adipisci enim sed quia
+                            eius et nemo possimus qui laboriosam incidunt non quisquam cupiditate quo
+                            omnis dolores. Est laboriosam dolore et assumenda numquam At numquam porro
+                            hic voluptas odit ut quas reiciendis ea libero consequatur. Est quia ullam qui
+                            unde quasi aut omnis internos!</p>
+                    </div>
+                </div>
+                <div className="contents-game">
+                    <div className="content-img">
+                        <img src="/assets/theGame/1.jfif" alt="" />
+                    </div>
+                    <div className="content-p">
+                        <h1>Content title</h1>
+                        <p>Lorem ipsum dolor sit amet. Et incidunt sequi aut adipisci enim sed quia
+                            eius et nemo possimus qui laboriosam incidunt non quisquam cupiditate quo
+                            omnis dolores. Est laboriosam dolore et assumenda numquam At numquam porro
+                            hic voluptas odit ut quas reiciendis ea libero consequatur. Est quia ullam qui
+                            unde quasi aut omnis internos! Lorem ipsum dolor sit amet. Et incidunt sequi aut adipisci enim sed quia
+                            eius et nemo possimus qui laboriosam incidunt non quisquam cupiditate quo
+                            omnis dolores. Est laboriosam dolore et assumenda numquam At numquam porro
+                            hic voluptas odit ut quas reiciendis ea libero consequatur. Est quia ullam qui
+                            unde quasi aut omnis internos!</p>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    );
+}
+
+export default TheGame;
